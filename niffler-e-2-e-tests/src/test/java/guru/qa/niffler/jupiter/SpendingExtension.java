00bf39329e0d7be7e4566e63c36a061c76c28f9b
vasvap <vasvap@gmail.com>
2024-12-25 19:53:14 +0300

    первое занятие

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/SpendingExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/SpendingExtension.java
@@ -11,14 +11,19 @@ import java.util.Date;
 
 public class SpendingExtension implements BeforeEachCallback, ParameterResolver {
 
+    // NAMESPACE - уникальный идентификатор конкретного extension
     public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(SpendingExtension.class);
 
     private final SpendApiClient spendApiClient = new SpendApiClient();
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
+        // ищем тестовый метод через контекст context.getRequiredTestMethod() у которого есть аннотация Spending при помощи метода findAnnotation
+        // метод findAnnotation возвращает тип Optional (микро коллекция - контейнер для одного элемента, который либо есть, либо его нет)
         AnnotationSupport.findAnnotation(context.getRequiredTestMethod(), Spending.class)
+                // метод ifPresent будет вызван только в том случае, если у теста нашлась аннотация Spending
                 .ifPresent(anno -> {
+                    // создаем объект SpendJson - это POJO объект, объект который нужно серилизовать и отправить на back (Java record)
                     SpendJson spend = new SpendJson(
                             null,
                             new Date(),
@@ -33,9 +38,19 @@ public class SpendingExtension implements BeforeEachCallback, ParameterResolver 
                             anno.description(),
                             anno.username()
                     );
+                    // создаем spending через API (возвращает объект - созданный spending с id)
+                    SpendJson createdSpend = spendApiClient.createSpend(spend);
+                    // кладем созданный spending в контекст:
+                    // getStore - создаем хранилище (похож на Map, данные хранятся ключ-значение), которое будет
+                    // предназначено для хранения данных работы данного extension (SpendingExtension)
+                    // на вход getStore передается NAMESPACE (уникальный идентификатор extension)
+                    // в getStore можно передавать разные NAMESPACE и каждый раз будет создаваться новое хранилище (может быть много)
                     context.getStore(NAMESPACE).put(
+                            // ключ - getUniqueId() (метод которые есть в ExtensionContext) уникальный идентификатор теста
+                            // никогда не может быть пустым
                             context.getUniqueId(),
-                            spendApiClient.createSpend(spend)
+                            // значение - созданные spending
+                            createdSpend
                     );
                 });
     }
