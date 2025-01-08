299a8deddf298d0c3b961d858283c7852ac8c042
vasvap <vasvap@gmail.com>
2025-01-08 19:23:58 +0300

    lesson-04

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java
@@ -9,8 +9,28 @@ enum LocalConfig implements Config {
     }
 
     @Override
+    public String authUrl() {
+        return "http://127.0.0.1:9000/";
+    }
+
+    @Override
+    public String authJdbcUrl() {
+        return "jdbc:postgresql://127.0.0.1:5432/niffler-auth";
+    }
+
+    @Override
     public String spendUrl() {
         return "http://127.0.0.1:8093/";
+    }
+
+    @Override
+    public String spendJdbcUrl() {
+        return "jdbc:postgresql://localhost:5432/niffler-spend";
+    }
+
+    @Override
+    public String currencyJdbcUrl() {
+        return "jdbc:postgresql://localhost:5432/niffler-currency";
     }
 
     @Override
@@ -26,5 +46,10 @@ enum LocalConfig implements Config {
     @Override
     public String userdataUrl() {
         return "http://127.0.0.1:8089/";
+    }
+
+    @Override
+    public String userdataJdbcUrl() {
+        return "jdbc:postgresql://localhost:5432/niffler-userdata";
     }
 }
